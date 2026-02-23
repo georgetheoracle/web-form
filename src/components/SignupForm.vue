a11acada691d93035feb9e5ba17ee8dd2036cbe3
George Octavius <113178814+georgetheoracle@users.noreply.github.com>
2026-02-23 14:57:43 -0500

    update

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -9,14 +9,23 @@
         <option value="admin">Admin</option>
         <option value="user">User</option>
     </select>
-    <div class="terms">
-        <input type="checkbox" id="terms" required>
-        <label for="terms">I agree to the terms and conditions</label>
+
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+    <div v-for="skill in skills" :key="skill" class="pill">
+        <span @click="deleteSkill(skill)">{{ skill }}</span>
     </div>
+
+    <div class="terms">
+        <input type="checkbox" v-model="terms" required>
+        <label>I agree to the terms and conditions</label>
+    </div>
+    
   </form>
     <p>Email: {{ email }} </p>
     <p>Password: {{ password }} </p>
     <p>Role: {{ role }} </p>
+    <p>Agreed to terms: {{ terms }} </p>
 </template>
 
 <script>
@@ -25,8 +34,25 @@ data() {
     return {
         email: '',
         password: '',
-        role: ''
+        role: '',
+        terms: false,
+        tempSkill: '',
+        skills: []
     }
+  },
+  methods:{
+    addSkill(e){
+        if(e.key === ',' && this.tempSkill){
+            if(!this.skills.includes(this.tempSkill)){
+            this.skills.push(this.tempSkill)
+            }
+            this.tempSkill = '';
+        }
+    },
+    deleteSkill(skill){
+        this.skills = this.skills.filter(s => s !== skill);
+    }
+
   }
 }
 </script>
@@ -64,5 +90,17 @@ input[type="checkbox"]{
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+}
+.pill{
+    display:inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
 }
 </style>
