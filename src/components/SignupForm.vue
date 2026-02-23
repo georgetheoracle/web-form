9b1b82942ff81a7bbd3fe2499b5b21a073ae6837
George Octavius <113178814+georgetheoracle@users.noreply.github.com>
2026-02-23 13:03:12 -0500

    update

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,68 @@
+<template>
+  <form >
+    <label for="email">Email:</label>
+    <input type="email" required v-model="email">
+    <label for="password">Password:</label>
+    <input type="password" required v-model="password">
+    <label>Role:</label>
+    <select v-model="role">
+        <option value="admin">Admin</option>
+        <option value="user">User</option>
+    </select>
+    <div class="terms">
+        <input type="checkbox" id="terms" required>
+        <label for="terms">I agree to the terms and conditions</label>
+    </div>
+  </form>
+    <p>Email: {{ email }} </p>
+    <p>Password: {{ password }} </p>
+    <p>Role: {{ role }} </p>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        email: '',
+        password: '',
+        role: ''
+    }
+  }
+}
+</script>
+
+<style>
+form{
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label{
+    color:#aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input, select{
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+input[type="checkbox"]{
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+}
+</style>
